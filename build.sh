a68915c4d7bbd937aa90160550cc0a99b39cfbdc
jokulhaups <jokullmani12@gmail.com>
2022-01-14 16:50:21 +0000

    Made disabling rust warnings more easily configurable

--- build.sh
+++ build.sh
@@ -4,9 +4,11 @@ set -xe
 
 SRC_DIR=src
 TARGET_DIR=target
-DISABLED_WARNINGS=dead_code
+DISABLED_LINTS="dead_code unused_variables"
 
-rustc -O -A $DISABLED_WARNINGS --edition 2021 $SRC_DIR/main.rs -o $TARGET_DIR/main
+LINTER_SETTINGS=$(for lint in $DISABLED_LINTS; do printf ' -A %s' $lint; done)
+
+rustc -O $LINTER_SETTINGS --edition 2021 $SRC_DIR/main.rs -o $TARGET_DIR/main
 
 if [ $1 = "run" ]
 then
